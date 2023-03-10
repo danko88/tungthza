c71b6f09174f032291101f1e08302b84b7fcceaa
danko88 <127289771+danko88@users.noreply.github.com>
2023-03-10 22:39:51 +0700

    Update stratum_mining.sh

--- stratum_mining.sh
+++ stratum_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS to run the miner"
 while :; do
-    ./astrominer -w YOUR_ADDRESS -r stratum+tls://pool.whalesburg.com:4300 -p stratum;
+    ./astrominer -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnjav4dupf0emd -r nodent2.cpumining.cloud:10100 r1 community-pool.mysrv.cloud:10300 -r2 dero.rabidmining.com:10300 -p rpc
     sleep 5;
 done
